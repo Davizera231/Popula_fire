c0185108a97192a8e97cc484a9bc0cb6a8187015
Davizera231 <dsandesteixeira54@gmail.com>
2025-11-21 09:25:55 -0300

    feature: Adicionando progress bar e formatando readme.md

--- Converter.py
+++ Converter.py
@@ -1,14 +1,35 @@
-import pandas as pd 
+import pandas as pd
+import time
+from tqdm import tqdm 
+import os
 
-CSV_FILE = r"C:\Users\dsand\OneDrive\Área de Trabalho\Populate_Firebase\CSV\Dados.xlsx"
+XLSX_FILE = r"C:\Users\dsand\OneDrive\Área de Trabalho\Populate_Firebase\CSV\Dados.xlsx"
+CSV_FILE = r"C:\Users\dsand\OneDrive\Área de Trabalho\Populate_Firebase\CSV\Dados.csv"
 
 def LerCsv():
-    print("Iniciando Leitura do CSV...")
+    print("Iniciando conversão e leitura...")
+
+    inicio = time.perf_counter()
+
     
-    Document = pd.read_excel(CSV_FILE)
+    df = pd.read_excel(XLSX_FILE, dtype=str)
+    time.sleep(0.3)
+    tqdm(total = 100).update(100)
 
-    print(f"{(Document.to_string())} Registros encontrados no arquivo Excel.")
+    
+    print("Convertendo para CSV...")
+    for i in tqdm(range(100)):
+        time.sleep(0.005)
+    df.to_csv(CSV_FILE, index=False, encoding="utf-8")
 
-    return Document
+    
 
+    df_csv = pd.read_csv(CSV_FILE, dtype=str)
+    for i in tqdm(range(100)):
+        time.sleep(0.005)
 
+    fim = time.perf_counter()
+
+    print(f"Processo concluído em {fim - inicio:.4f} segundos.")
+
+    return df_csv
